5d4e1b67ca2214926651b1ee497eed67aabfcf39
kazacoder <avtk.tlt@gmail.com>
2022-10-16 20:38:16 +0300

    vue changed

--- testvue.js
+++ testvue.js
@@ -1,6 +1,48 @@
-const { createApp } = Vue
+const {createApp} = Vue
 
-createApp ({
+createApp({
+    data() {
+        return {
+            title: 'Список заметок',
+            placeholderString: 'Введите название заметки',
+            inputValue: '',
+            notes: ['Заметка 1', 'Заметка 2']
+        }
+    },
+    methods: {
+        inputChangeHandler(event) {
+            this.inputValue = event.target.value
+        },
+        addNewNote() {
+            if (this.inputValue !== '') {
+                this.notes.push(this.inputValue)
+                this.inputValue = ''
+            }
+        },
+        toUpperCase(item) {
+            return item.toUpperCase()
+        },
+        removeNote(index) {
+            this.notes.splice(index, 1)
+        }
+    },
+    computed: {
+        doubleCountComputed() {
+            console.log('doubleCountComputed')
+            return this.notes.length * 2
+        }
+    },
+    watch: {
+        inputValue(value) {
+            if (value.length > 10) {
+                this.inputValue = ''
+            }
+            console.log('input Value changed' ,value)
+        }
+    }
+}).mount('#appVueInput')
+
+createApp({
     data() {
         return {
             message: 'Hello Vue!',
@@ -8,3 +50,4 @@ createApp ({
         }
     }
 }).mount('#appVue')
+
